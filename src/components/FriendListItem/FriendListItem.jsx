b9613ad8f5b53015389cca0c6f787eca421c2877
Oleksandr <kulykof@gmail.com>
2023-01-18 01:10:42 +0200

    Add some changes

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -27,9 +27,3 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
     </li>
   );
 };
-
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-};
